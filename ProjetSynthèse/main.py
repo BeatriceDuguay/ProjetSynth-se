cd5f61f5bd0e38ede92e08c34fa6c1ba8d5f2235
Béatrice Duguay <1724602>
2022-05-08 15:32:26 -0400

    Commit #4 : Ajout des importations dans le programme principal

--- ProjetSynthèse/main.py
+++ ProjetSynthèse/main.py
@@ -2,34 +2,49 @@
 ###  IMPORTATIONS - Portée globale  ###
 #######################################
 
+# importer le module sys nécessaire à l'exécution.
+import sys
+
+# pour la réinitialisation de la date dans le dateEdit
+from PyQt5.QtCore import QDate
+
+# importer pour le model de la listView
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+
+# importer l'interface graphique
+from interfacegraphique import *
+
+# importer les dialogues
+from dialogueEmprunt import *
+from dialogueDetailsEmprunt import *
+from dialogueProduitFilm import *
+from dialogueProduitJeu import *
+from dialogueProduitLivre import *
+
+# importer la classe Produit
 from Produit import  *
+
+# importer la classe Film
 from Film import  *
+
+# importer la classe Livre
 from Livre import  *
+
+# importer la classe Jeu
 from Jeu import  *
+
+# importer la classe Abonne
 from Abonne import  *
+
+# importer la classe Emprunt
 from Emprunt import  *
-from DetailEmprunt import  *
 
-###############################
-#####  MESSAGE IMPORTANT  #####
-###############################
+# importer la classe DetailsEmprunt
+from DetailsEmprunt import  *
 
-# Je n'ai pas réussi à faire les tests. En utilisant le debugger, j'ai remarquer que Python ne va pas voir le setter et
-# va directement dans le getter.
 
-#############################
-#####       TESTS       #####
-#############################
 
-# tests de la classe mère Produit
 
-# la propriété __numeroSerie n'est pas respecté
-Produit1 = Produit("Livre", "BBBB4822949392", "POO 2", "Français", 1999, "Drame")
-
-if Produit1.NumeroSerie == "":
-    print("Erreur")
-else:
-    print(Produit1)
 
 
 
