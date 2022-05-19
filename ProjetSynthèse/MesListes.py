a9eaf4965ddc7cfccb63d49c95a68cdef72a6eef
Béatrice Duguay <1724602>
2022-05-19 19:15:16 -0400

    Commit #14 : ajout du fichier .py MesListes pour l'importation des listes

---
+++ ProjetSynthèse/MesListes.py
@@ -0,0 +1,22 @@
+####################################################################################
+###  420-2G2-HU - Programmation orientée objet
+###  Travail: Projet synthèse
+###  Nom: Béatrice Duguay
+###  No étudiant: 1724602
+###  No Groupe: 002
+###  Description du fichier: Listes à importer
+####################################################################################
+
+# déclaration de la liste liste_abonnes
+liste_abonnes = []
+
+# déclaration de la liste liste_produit
+liste_produits = []
+
+# déclaration de la liste liste_detail_emprunts
+liste_detail_emprunts = []
+
+# déclaration de la liste liste_emprunt
+liste_emprunts = []
+
+
